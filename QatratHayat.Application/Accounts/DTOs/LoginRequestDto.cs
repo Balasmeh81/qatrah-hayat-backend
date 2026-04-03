003c97598532f70dcfdb4ec5f0bcf5277a4c7c35
Balasmeh81 <abdalrahmanbalasmeh81@gmail.com>
2026-04-04 01:05:15 +0300

    Create National Table & Implement initial auth flow with register and login

---
+++ QatratHayat.Application/Accounts/DTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace QatratHayat.Application.Accounts.DTOs
+{
+    public class LoginRequestDto
+    {
+        public string EmailOrNationalId { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
